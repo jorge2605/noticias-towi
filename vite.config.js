eb02503d05352a6d9d2ffa4190af4f2290d83be6
JORGE SANTACRUZ <santacruz.leonel.1h@hotmail.com>
2024-10-04 12:52:15 -0600

    Configuracion para subirlo a pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",  // Permite que el servidor sea accesible desde la red local
-    port: 3000,  // El puerto que estás utilizando
-  },
+  base: '/noticias-towi/',
 })
